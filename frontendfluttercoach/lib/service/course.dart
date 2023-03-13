773ef90b5c63fd98ad271dda982102e6b5c45f2b
PondXIV <114280608+PondXIV@users.noreply.github.com>
2023-03-13 16:56:36 +0700

    update

---
+++ frontendfluttercoach/lib/service/course.dart
@@ -0,0 +1,16 @@
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+import '../model/modelCourse.dart';
+
+part 'course.g.dart';
+
+@RestApi()
+abstract class CourseService {
+  factory CourseService(Dio dio, {String baseUrl}) = _CourseService;
+
+  @GET("/course/getCourseByIDCoach/{cid}")
+  Future<HttpResponse<ModelCourse>> getCoachByCid(@Path("cid") String cid);
+
+ 
+}
