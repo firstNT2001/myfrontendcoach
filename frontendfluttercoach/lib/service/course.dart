8c0b2419e3083dc5b4e75045fed2ae63466553e9
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-03-17 11:00:14 +0700

    update

--- frontendfluttercoach/lib/service/course.dart
+++ frontendfluttercoach/lib/service/course.dart
@@ -3,25 +3,6 @@ import 'package:dio/dio.dart';
 
 import '../model/modelCourse.dart';
 
-part 'course.g.dart';
-
-
-@RestApi()
-abstract class CourseService {
-  factory CourseService(Dio dio, {String baseUrl}) = _CourseService;
-
-  @GET("/course/getCourseByIDCoach/{cid}")
-  Future<HttpResponse<ModelCourse>> getCoachByCid(@Path("cid") String cid);
-
-  
-
- 
-}
-import 'package:retrofit/retrofit.dart';
-import 'package:dio/dio.dart';
-
-import '../model/modelCourse.dart';
-
 part 'generated/course.g.dart';
 
 
