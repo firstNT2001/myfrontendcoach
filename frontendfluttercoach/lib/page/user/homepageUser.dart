647db9cef06627fd471c5a370a95179ff0fbe630
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-03-20 18:30:43 +0700

    uppdateError

--- frontendfluttercoach/lib/page/user/homepageUser.dart
+++ frontendfluttercoach/lib/page/user/homepageUser.dart
@@ -3,11 +3,16 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:frontendfluttercoach/model/modelCoach.dart';
+import 'package:frontendfluttercoach/page/user/profilecoach.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:retrofit/dio.dart';
 import 'dart:developer';
+import '../../model/modelCourse.dart';
 import '../../service/coach.dart';
+import '../../service/course.dart';
 import '../../service/provider/appdata.dart';
+import 'cousepage.dart';
 
 class HomePageUser extends StatefulWidget {
   const HomePageUser({super.key});
@@ -18,20 +23,25 @@ class HomePageUser extends StatefulWidget {
 
 class _HomePageUserState extends State<HomePageUser> {
   late CoachService coachService;
+  late CourseService courseService;
   List<Coach> coaches = [];
-  //List<Coach> coaches = [];
+  List<ModelCourse> courses = [];
   TextEditingController myController = TextEditingController();
+  bool isVisible = false;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    coachService =
+        coachService =
         CoachService(Dio(), baseUrl: context.read<AppData>().baseurl);
+    courseService =
+        CourseService(Dio(), baseUrl: context.read<AppData>().baseurl);
   }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text("tesr"),
@@ -61,12 +71,30 @@ class _HomePageUserState extends State<HomePageUser> {
                         if (myController.text.isNotEmpty) {
                           coachService
                               .getNameCoach(myController.text)
-                              .then((data) {
-                            var datacoach = data.data;
+                              .then((coachdata) {
+                            var datacoach = coachdata.data;
+                            //var checkcoaches = coaches.length;
                             coaches = datacoach;
-                            if (coaches != null) {
-                              setState(() {});
+                            if (coaches.isNotEmpty) {
+                              setState(() {
+                                isVisible = true;
+                              });
+
                               log(coaches.length.toString());
+                            } else {
+                              setState(() {
+                                isVisible = false;
+                              });
+                            }
+                          });
+                          courseService
+                              .getCoachByName(myController.text)
+                              .then((coursedata) {
+                            var datacourse = coursedata.data;
+                            courses = datacourse;
+                            if (courses.isNotEmpty) {
+                              setState(() {});
+                              log(courses.length.toString());
                             }
                           });
                         }
@@ -82,25 +110,70 @@ class _HomePageUserState extends State<HomePageUser> {
               IconButton(onPressed: () {}, icon: Icon(Icons.tune_rounded)),
             ],
           ),
-          (coaches != null)
+
+           Visibility(
+                  visible: isVisible ,
+                  child: Expanded(
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: coaches.length,
+                            itemBuilder: (context, index) {
+                              final coach = coaches[index];
+                              return Card(
+                                child: ListTile(
+                                  leading: CircleAvatar(
+                                    radius: 50,
+                                    backgroundImage: NetworkImage(coach.image),
+                                  ),
+                                  title: Text(coach.cid.toString()),
+                                  subtitle: Text(coach.fullName),
+                                  trailing: const Icon(Icons.arrow_forward),
+                                  onTap: () {
+                                    log(coach.cid.toString());
+                                    context.read<AppData>().cid = coach.cid;
+                                    Get.to(()=>ProfileCoachPage());
+
+                                  },
+                                ),
+                              );
+                            }),
+                      ),
+                    ),
+                  ),
+                ),
+            
+          //showCourse
+          (courses != null)
               ? Expanded(
                   child: Container(
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(20.0),
                       child: ListView.builder(
-                        itemCount: coaches.length,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Column(
-                              children: [
-                                Text(coaches[index].username),
-                                Text(coaches[index].fullName)
-                              ],
-                            ),
-                          );
-                        },
-                      ),
+                          shrinkWrap: true,
+                          itemCount: courses.length,
+                          itemBuilder: (context, index) {
+                            final course = courses[index];
+                            return Card(
+                              child: ListTile(
+                                leading: CircleAvatar(
+                                  radius: 30,
+                                  backgroundImage: NetworkImage(course.image),
+                                ),
+                                title: Text(course.name),
+                                subtitle: Text(course.details),
+                                trailing: const Icon(Icons.arrow_forward),
+                                onTap: () {
+                                  Navigator.of(context).push(MaterialPageRoute(
+                                    builder: (context) =>
+                                        showCousePage(couse: course ),
+                                  ));
+                                },
+                              ),
+                            );
+                          }),
                     ),
                   ),
                 )
