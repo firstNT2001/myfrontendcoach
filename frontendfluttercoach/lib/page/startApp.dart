da4d2425a541fd0fd4ff9d33fa666b72fb653fd3
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-03-23 17:23:42 +0700

    update

--- frontendfluttercoach/lib/page/startApp.dart
+++ frontendfluttercoach/lib/page/startApp.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:frontendfluttercoach/page/user/homepageUser.dart';
 import 'package:get/get.dart';
 
-import 'coach/homePageCoach.dart';
+import 'coach/Homepagecoach.dart';
 
 class StartApp extends StatelessWidget {
   const StartApp({super.key});
