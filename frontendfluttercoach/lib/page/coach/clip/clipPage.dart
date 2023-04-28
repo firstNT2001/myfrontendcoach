5e2dcbe2e78152a05ec050facae68465098c567f
PondXIV <114280608+PondXIV@users.noreply.github.com>
2023-04-28 17:30:33 +0700

    commit

---
+++ frontendfluttercoach/lib/page/coach/clip/clipPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClipPage extends StatefulWidget {
+  const ClipPage({super.key});
+
+  @override
+  State<ClipPage> createState() => _ClipPageState();
+}
+
+class _ClipPageState extends State<ClipPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
