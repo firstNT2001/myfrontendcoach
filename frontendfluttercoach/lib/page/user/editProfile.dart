ffeabf68ffaada16861e75f4856e5aff33d1ef27
firstNT2001 <114280701+firstNT2001@users.noreply.github.com>
2023-04-30 23:11:27 +0700

    eror

--- frontendfluttercoach/lib/page/user/editProfile.dart
+++ frontendfluttercoach/lib/page/user/editProfile.dart
@@ -1,13 +1,13 @@
+import 'dart:convert';
 import 'dart:developer';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:retrofit/retrofit.dart';
-
 import '../../model/modelCustomer.dart';
 import '../../service/customer.dart';
 import '../../service/provider/appdata.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class editProfileCus extends StatefulWidget {
   //สร้างตัวแปรรับconstructure
@@ -33,11 +33,14 @@ class _editProfileCusState extends State<editProfileCus> {
   TextEditingController _birthday = TextEditingController();
   TextEditingController _gender = TextEditingController();
   TextEditingController _phone = TextEditingController();
-  TextEditingController _image = TextEditingController();
   TextEditingController _weight = TextEditingController();
   TextEditingController _height = TextEditingController();
   TextEditingController _facebookID = TextEditingController();
-  TextEditingController _price = TextEditingController();
+  int _price = 0;
+
+  final List<String> genders = ['ผู้หญิง', 'ผู้ชาย'];
+  
+  String _image = " ";
 
   @override
   void initState() {
@@ -70,16 +73,21 @@ class _editProfileCusState extends State<editProfileCus> {
       _uid.text = customer.data.uid.toString();
       _username.text = customer.data.username;
       _fullName.text = customer.data.fullName;
+      _birthday.text = customer.data.birthday;
+      _gender.text = customer.data.gender;
+      _phone.text = customer.data.phone;
       _email.text = customer.data.email;
       _password.text = customer.data.password;
-      _image.text = customer.data.image;
       _facebookID.text = customer.data.facebookId;
-      _price.text = customer.data.price.toString();
-
-      log('cus: ${customer.data.uid}');
-      log('_username: ${customer.data.username}');
-      log('_email: ${customer.data.email}');
-      log('message: ${customer.data.fullName}');
+      _price = customer.data.price;
+      _weight.text = customer.data.weight.toString();
+      _height.text = customer.data.height.toString();
+      //gender show
+      if(_gender.text =="1"){
+        _gender.text = "ชาย";
+      }else{
+        _gender.text = "หญิง";
+      }
     } catch (err) {
       log('Error: $err');
     }
@@ -113,18 +121,139 @@ class _editProfileCusState extends State<editProfileCus> {
               padding: const EdgeInsets.all(20.0),
               child: Column(
                 children: [
+                  CircleAvatar(
+                              minRadius: 35,
+                              maxRadius: 55,
+                              backgroundImage:
+                                  NetworkImage(customer.data.image),
+                            ),
                   txtfild(_username, "ชื่อผู้ใช้", "ชื่อผู้ใช้"),
                   txtfild(_email, "e-mail", "e-mail"),
                   txtfild(_fullName, "ชื่อ-นามสกุล", "ชื่อ-นามสกุล"),
-                  txtfild(_gender, "เพศ", "เพศ"),
+                  buildDropdownGender(),
                   txtfild(_phone, "โทรศัพท์", "โทรศัพท์"),
                   txtfild(_weight, "น้ำหนัก", "น้ำหนัก"),
                   txtfild(_height, "ส่วนสูง", "ส่วนสูง"),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15,bottom: 15),
+                    child: ElevatedButton(child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text("บันทึก"),
+                    ),onPressed: ()async{
+                      Customer updateCustomer = Customer(
+                        uid: widget.uid, 
+                        username: _username.text, 
+                        password: _password.text, 
+                        email: _email.text, 
+                        fullName: _fullName.text, 
+                        birthday: _birthday.text, 
+                        gender: _gender.text, 
+                        phone: _phone.text, 
+                        image: _image, 
+                        weight: int.parse(_weight.text), 
+                        height: int.parse(_height.text), 
+                        facebookId: _facebookID.text, 
+                        price: _price, 
+                        buying: 2);
+                        log(jsonEncode(updateCustomer));
+                        updateCustomer =
+                      (await customerService.updateCus(cusUpdate)) as Customer;
+
+                    } ),
+                  )
                   
                 ],
               ),
             );
           }
         });
+  }
+  String? selectGender;
+  List<DropdownMenuItem<String>> _addDividersAfterGender(List<String> genders) {
+    List<DropdownMenuItem<String>> _genders = [];
+    for (var gender in genders) {
+      _genders.addAll(
+        [
+          DropdownMenuItem<String>(
+            value: gender,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                gender,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ),
+          ),
+          //If it's last item, we will not add Divider after it.
+          if (gender != genders.last)
+            const DropdownMenuItem<String>(
+              enabled: false,
+              child: Divider(),
+            ),
+        ],
+      );
+    }
+    return _genders;
+  }
+
+  List<double> _getCustomGenderHeights() {
+    List<double> _gendersHeights = [];
+    for (var i = 0; i < (genders.length * 2) - 1; i++) {
+      if (i.isEven) {
+        _gendersHeights.add(40);
+      }
+      //Dividers indexes will be the odd indexes
+      if (i.isOdd) {
+        _gendersHeights.add(4);
+      }
+    }
+    return _gendersHeights;
+  }
+    Widget buildDropdownGender() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'เพศ',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        DropdownButtonHideUnderline(
+          child: DropdownButton2(
+            isExpanded: true,
+            hint: Text(
+              _gender.text,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            items: _addDividersAfterGender(genders),
+            value: selectGender,
+            onChanged: (value) {
+              //log("5555"+selectedValue);
+
+              setState(() {
+                selectGender = value as String;
+
+                log("va= " + value.toString());
+              });
+              log(customer.data.gender);
+            },
+            buttonStyleData: ButtonStyleData(
+              //height: 33,
+              padding: const EdgeInsets.only(left: 14, right: 14),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                border: Border.all(width: 1),
+              ),
+            ),
+            dropdownStyleData: const DropdownStyleData(
+              maxHeight: 200,
+            ),
+            menuItemStyleData: MenuItemStyleData(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              customHeights: _getCustomGenderHeights(),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
