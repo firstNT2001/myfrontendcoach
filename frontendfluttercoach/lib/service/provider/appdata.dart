6cee3fc4fd05a0c325689bb306ae4d590f956c9c
PondXIV <114280608+PondXIV@users.noreply.github.com>
2023-06-18 15:14:11 +0700

    commit

--- frontendfluttercoach/lib/service/provider/appdata.dart
+++ frontendfluttercoach/lib/service/provider/appdata.dart
@@ -13,7 +13,8 @@ import '../request.dart';
 
 class AppData with ChangeNotifier {
   //Api baseurl
-  String baseurl = "http://202.28.34.197:9775";
+  //String baseurl = "http://202.28.34.197:9775";
+  String baseurl = "https://cslab.it.msu.ac.th:9775";
 
   Map<String,dynamic> userFacebook = {} ; 
   int coID = 0;
