81464403afbd1d719ab160ee74583d53a5554261
firstNT2001 <thonthai116@gmail.com>
2023-08-07 14:20:53 +0700

    commit

---
+++ frontendfluttercoach/lib/page/user/mycourse.Detaildart/Widget/widget_progessnar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Widgetprogessbar extends StatefulWidget {
+  const Widgetprogessbar({super.key});
+
+  @override
+  State<Widgetprogessbar> createState() => _WidgetprogessbarState();
+}
+
+class _WidgetprogessbarState extends State<Widgetprogessbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
