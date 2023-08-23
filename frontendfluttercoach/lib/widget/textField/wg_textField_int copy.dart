bdbcc6085f293d5bf81a918d8d0192c15cff4a17
PondXIV <114280608+PondXIV@users.noreply.github.com>
2023-08-23 17:04:15 +0700

    commit

--- frontendfluttercoach/lib/widget/textField/wg_textField_int copy.dart
+++ frontendfluttercoach/lib/widget/textField/wg_textField_int copy.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:string_validator/string_validator.dart';
 
 class WidgetTextFieldInt extends StatefulWidget {
   final TextEditingController controller;
@@ -16,31 +19,66 @@ class WidgetTextFieldInt extends StatefulWidget {
 }
 
 class _WidgetTextFieldIntState extends State<WidgetTextFieldInt> {
+  late   bool isValidw;
+  late   bool isValidh;
+    bool _isvisibleHW = false;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:  const EdgeInsets.only(bottom: 10, left: 15, right: 15),
+      padding: const EdgeInsets.only(bottom: 10, left: 15, right: 15),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.only(left: 5, bottom: 3),
-            child: Text(widget.labelText, style: Theme.of(context).textTheme.bodyLarge,),
+            child: Text(
+              widget.labelText,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
           ),
-          TextField(
+          TextFormField(
               keyboardType: TextInputType.number,
               controller: widget.controller,
-              style: Theme.of(context).textTheme.bodyLarge,
-              maxLength: widget.maxLength,
-      
-              //inputFormatters: [LengthLimitingTextInputFormatter(widget.maxLength)],
+              validator: (value) {
+                isValidw = isNumeric(value!); // false
+                log(isValidw.toString());
+                if (isValidw == true) {
+                  log("BB");
+                } else {
+                  log("FF");
+                  setState(() {
+                    _isvisibleHW = true;
+                    log("PP");
+                  });
+                }
+
+                return null;
+              },
+              maxLength:  widget.maxLength,
               textAlignVertical: TextAlignVertical.center,
               textAlign: TextAlign.center,
               decoration: InputDecoration(
-                contentPadding: EdgeInsets.symmetric(vertical: 9, horizontal: 12),
-                   counterText: "",
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 9, horizontal: 12),
+                  counterText: "",
                   filled: true,
                   fillColor: Theme.of(context).colorScheme.background)),
+          // TextField(
+          //     keyboardType: TextInputType.number,
+          //     controller: widget.controller,
+          //     style: Theme.of(context).textTheme.bodyLarge,
+          //     maxLength: widget.maxLength,
+
+          //     //inputFormatters: [LengthLimitingTextInputFormatter(widget.maxLength)],
+          //     textAlignVertical: TextAlignVertical.center,
+          //     textAlign: TextAlign.center,
+          //     decoration: InputDecoration(
+          //         contentPadding:
+          //             EdgeInsets.symmetric(vertical: 9, horizontal: 12),
+          //         counterText: "",
+          //         filled: true,
+          //         fillColor: Theme.of(context).colorScheme.background)),
         ],
       ),
     );
