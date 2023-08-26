a119b8ad4fa603d0c8fc98bd73b1330c50de4705
firstNT2001 <thonthai116@gmail.com>
2023-08-26 16:38:29 +0700

    update

--- frontendfluttercoach/lib/service/generated/review.g.dart
+++ frontendfluttercoach/lib/service/generated/review.g.dart
@@ -72,6 +72,30 @@ class _ReviewService implements ReviewService {
     return httpResponse;
   }
 
+  @override
+  Future<HttpResponse<Scorecoach>> scorecoach(cid) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    final _result = await _dio.fetch<Map<String, dynamic>>(
+        _setStreamType<HttpResponse<Scorecoach>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/review/${cid}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    final value = Scorecoach.fromJson(_result.data!);
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
