42cf50843f8e256a69227c198c61333a2041e433
firstNT2001 <thonthai116@gmail.com>
2023-08-26 15:51:00 +0700

    update

--- frontendfluttercoach/lib/model/response/md_Review_get.dart
+++ frontendfluttercoach/lib/model/response/md_Review_get.dart
@@ -5,6 +5,8 @@
 import 'package:meta/meta.dart';
 import 'dart:convert';
 
+import 'md_Customer_get.dart';
+
 List<ModelReview> modelReviewFromJson(String str) => List<ModelReview>.from(json.decode(str).map((x) => ModelReview.fromJson(x)));
 
 String modelReviewToJson(List<ModelReview> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
@@ -49,66 +51,3 @@ class ModelReview {
     };
 }
 
-class Customer {
-    int uid;
-    String username;
-    String password;
-    String email;
-    String fullName;
-    DateTime birthday;
-    String gender;
-    String phone;
-    String image;
-    int weight;
-    int height;
-    String facebookId;
-    int price;
-
-    Customer({
-        required this.uid,
-        required this.username,
-        required this.password,
-        required this.email,
-        required this.fullName,
-        required this.birthday,
-        required this.gender,
-        required this.phone,
-        required this.image,
-        required this.weight,
-        required this.height,
-        required this.facebookId,
-        required this.price,
-    });
-
-    factory Customer.fromJson(Map<String, dynamic> json) => Customer(
-        uid: json["Uid"],
-        username: json["Username"],
-        password: json["Password"],
-        email: json["Email"],
-        fullName: json["FullName"],
-        birthday: DateTime.parse(json["Birthday"]),
-        gender: json["Gender"],
-        phone: json["Phone"],
-        image: json["Image"],
-        weight: json["Weight"],
-        height: json["Height"],
-        facebookId: json["FacebookID"],
-        price: json["Price"],
-    );
-
-    Map<String, dynamic> toJson() => {
-        "Uid": uid,
-        "Username": username,
-        "Password": password,
-        "Email": email,
-        "FullName": fullName,
-        "Birthday": birthday.toIso8601String(),
-        "Gender": gender,
-        "Phone": phone,
-        "Image": image,
-        "Weight": weight,
-        "Height": height,
-        "FacebookID": facebookId,
-        "Price": price,
-    };
-}
